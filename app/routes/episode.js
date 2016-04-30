444d2efda99df0be2d379d20e8300c0a8b05e6d2
Taras Mankovski <tarasm@gmail.com>
2016-04-30 08:59:46 -0400

    Started working on Episode page

--- app/routes/episode.js
+++ app/routes/episode.js
@@ -5,6 +5,9 @@ export default Ember.Route.extend({
   model({slug}) {
     return this.store.queryRecord('episode', {
       filter: { name: slug }
+    }).then(function(model){
+      // TODO: replace this when is fixed https://github.com/emberjs/data/pull/4300#issuecomment-214479678
+      return model.get('firstObject');
     });
   }
   
