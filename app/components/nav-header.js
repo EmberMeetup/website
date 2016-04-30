e9c0a8a3b977a3843536936f4a2caaccd2a79911
Taras Mankovski <tarasm@gmail.com>
2016-04-30 18:09:19 -0400

    Made collapsible top menu

--- app/components/nav-header.js
+++ app/components/nav-header.js
@@ -1,5 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: 'header'
+  tagName: 'header',
+  classNameBindings: ['isCollapsible'],
+  isCollapsible: Ember.computed.or('media.isMobile', 'media.isTablet'),
+  media: Ember.inject.service()
 });
