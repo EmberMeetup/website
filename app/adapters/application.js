f568071a6a86b9fca8016cece81e322c9f77380a
Taras Mankovski <tarasm@gmail.com>
2016-05-10 17:42:39 -0400

    Switched to use //

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,7 @@
 import JSONAPIAdapter from 'ember-data/adapters/json-api';
 
 export default JSONAPIAdapter.extend({
-  host: 'http://embermeetup.com',
+  host: '//embermeetup.com',
   namespace: 'wp-json/wp/v2',
   findAll(store, type) {
     const url = this.buildURL(type.modelName, null, null, 'findAll');
