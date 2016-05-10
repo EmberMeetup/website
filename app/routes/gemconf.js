552595b27fa3e16b1c09fd18e4200713cb4045be
Taras Mankovski <tarasm@gmail.com>
2016-05-10 07:44:36 -0400

    Added breadcrumbs and style

--- app/routes/gemconf.js
+++ app/routes/gemconf.js
@@ -3,12 +3,14 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   
   model({slug}) {
+    
     return this.store.query('conference', {
       filter: { name: slug }
     }).then(function(model){
       // TODO: replace this when is fixed https://github.com/emberjs/data/pull/4300#issuecomment-214479678
       return model.get('firstObject');
     });
+    
   }
   
 });
