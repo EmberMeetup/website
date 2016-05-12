15bb5302895d14166e0b5d042723307deeb4f8d7
Taras Mankovski <tarasm@gmail.com>
2016-05-12 11:26:49 -0400

    API now always calling through https://

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,7 @@
 import JSONAPIAdapter from 'ember-data/adapters/json-api';
 
 export default JSONAPIAdapter.extend({
-  host: '//embermeetup.com',
+  host: 'https://embermeetup.com',
   namespace: 'wp-json/wp/v2',
   findAll(store, type) {
     const url = this.buildURL(type.modelName, null, null, 'findAll');
