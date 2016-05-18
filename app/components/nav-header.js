ae5b2994cee8d76f0fc7927692d42ebaf4bdb078
Taras Mankovski <tarasm@gmail.com>
2016-05-18 09:34:25 -0500

    Updated the header and added page titles

--- app/components/nav-header.js
+++ app/components/nav-header.js
@@ -2,8 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'header',
-  classNameBindings: ['isCollapsible'],
+  classNameBindings: [':nav-header', 'isCollapsible', 'headData.classNames', 'withHero:with-hero:no-hero'],
   isCollapsible: Ember.computed.or('media.isMobile', 'media.isTablet'),
   media: Ember.inject.service(),
-  headData: Ember.inject.service()
+  headData: Ember.inject.service(),
+  withHero: Ember.computed.notEmpty('headData.image')
 });
