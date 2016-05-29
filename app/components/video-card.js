685ff91ccd7fc22305c703a8007aef9678d9e1bf
Taras Mankovski <tarasm@gmail.com>
2016-05-29 14:26:06 -0400

    Added flag to video cards

--- app/components/video-card.js
+++ app/components/video-card.js
@@ -9,7 +9,8 @@ export default Ember.Component.extend({
     ':video-card', 
     'show-footer:has-footer', 
     'isSmall:is-small:is-not-small', 
-    'show-presenter:has-presenter'
+    'show-presenter:has-presenter',
+    'show-flag:show-flag:no-flag'
   ],
   isSmall: computed.equal('size', 'small'),
   "show-presenter": true
