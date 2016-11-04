4fa86d31aa75fa50e0d391fececdae8b797f2b2f
jorgelainfiesta <jorge.lainfiesta@gmail.com>
2016-11-04 07:39:44 -0600

    New page header.

--- app/routes/rock-and-roll-ember.js
+++ app/routes/rock-and-roll-ember.js
@@ -9,10 +9,7 @@ export default Ember.Route.extend({
   afterModel() {
     this.get('headData').change({
       title: 'Rock and Roll with Ember.js',
-      withHero: true,
-      heroImage: "assets/images/RARE-Logo.png",
-      heroSubTitle: "Complete hands-on introduction to authentication in Ember.js applications",
-      classNames: "rock-and-roll-ember"
+      withHero: false
     });
   }
 });
