94d71b1551c3cbe717d4369752d5e51f38b64c5b
Taras Mankovski <tarasm@gmail.com>
2016-11-11 10:40:44 -0800

    Added Google Analytics tracking (#32)

--- app/router.js
+++ app/router.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 import config from './config/environment';
+import Trackable from 'ember-cli-analytics/mixins/trackable';
 
-const Router = Ember.Router.extend({
+const Router = Ember.Router.extend(Trackable, {
   location: config.locationType,
   rootURL: config.rootURL
 });
